0276b9c6df3314f32d495dac84cb2c1cc1e1b10e
vitor-mota <vitormotanovais@gmail.com>
2025-09-11 23:21:14 -0300

    Adicionando modificações a barra de navegações referentes a mudança na estilização via scrollY

--- js/navBar.js
+++ js/navBar.js
@@ -1,10 +1,30 @@
 
 // script para modificar a barra de navegação quando rolar para baixo
 const navbar = document.getElementById("barraNavegacao");
+const logo = document.getElementById("logo")
+
+
+
 window.addEventListener("scroll", () =>{
     if (window.scrollY > 15){
         navbar.classList.add("scrolled")
+
+        if(!logo.src.includes("logoBarraNavegacaoWhiteColor.svg")){
+            logo.style.opacity = 0
+            setTimeout(() =>{
+                logo.src = "../assets/img/logoBarraNavegacaoWhiteColor.svg"
+                logo.style.opacity = 1
+            },200);
+        }
     }else{
         navbar.classList.remove("scrolled")
+
+        if(!logo.src.includes("logoBarraNavegacao.svg")){
+            logo.style.opacity = 0
+            setTimeout(() =>{
+                logo.src = "../assets/img/logoBarraNavegacao.svg"
+                logo.style.opacity = 1
+            },200)
+        }
     }
 })
